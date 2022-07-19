f7e24a9a7176ae993811733d7cf4b1489d240d78
Balaram Bera <bera.balaram514@gmail.com>
2022-07-19 16:19:28 +0530

    Removed Contact

--- contacts/commands.sh
+++
@@ -1,3 +0,0 @@
-docker run -d --name pg -e POSTGRES_PASSWORD=admin -e POSTGRES_USER=jiten -e POSTGRES_DB=sample -p 5432:5432 postgress
-
-go run main.go -stderrthreshold=FATAL
--- contacts/messaging/messaging.go
+++
@@ -1,8 +0,0 @@
-package messaging
-
-import "context"
-
-type IMessage interface {
-	Publish(context.Context) error
-	Subscribe(context.Context) error
-}
